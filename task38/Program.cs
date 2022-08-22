33ae65573c6bc22c19e3cc3ce8e8a70cd46c2d0f
Egor Еtrofimov <martynenkoa01@gmail.com>
2022-08-22 23:57:19 +0300

    final verion

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] array = new double[5];
+Random rand = new Random();
+
+double res = 0;
+double max = 0;
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(5, 27);
+
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+
+    }
+}
+res = max - min;
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(res);
+
+
+
